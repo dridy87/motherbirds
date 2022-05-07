0b7f18fe3f944815888b4f1422cc5104436c41cc
dridy87@gmail.com <dridy87@gmail.com>
2022-05-07 14:18:29 +0900

    다음 곡 자동 재생 추가

--- components/youtubu.js
+++ components/youtubu.js
@@ -10,29 +10,35 @@ export default function Example(props) {
     useEffect(() => {
 
         if(player != null){
-            player.playVideo()
+            let index = props.data.findIndex(t => t.videoId == props.videoId);
+            player.loadPlaylist(props.data.map(t=>t.videoId), index)
+            // player.playVideo()
         }
 
-        console.log(props)
+       
 
     });
 
     const onReady = (event) => {
-        console.log(`YouTube Player object for videoId: "${props.videoId}" has been saved to state.`);
-        setPlayer(event.target);
-        setReady(true);
+        if(props.videoId != null){
+            console.log(`YouTube Player object for videoId: "${props.videoId}" has been saved to state.`);
+            setPlayer(event.target);
+            setReady(true);
+        }
+        
     };
 
 
     const opts = {
         width: '100%',
         height: '300',
+        autoplay: 1
       };
 
 
     return (
         <div className="youtubu">
-            <YouTube opts={opts} videoId={props.videoId} onReady={onReady}  />
+            <YouTube opts={opts} videoId={props.videoId} onReady={onReady} />
         </div>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,105 +3,52 @@ import React, { useState, useEffect } from 'react';
 import YouTube from 'react-youtube';
 import { DataScroller } from 'primereact/datascroller';
 import YoutubePlayer from "../components/youtubu";
-import useSWR from 'swr';
+
 import store, { addTimeline } from '../common/store';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
+import Axios from "axios";
 const fetcher = (url) => fetch(url).then((res) => {
   return res.json()
 })
 // This function gets called at build time
 
-import {
-  useSession, signIn, signOut
-} from 'next-auth/client'
 
+import useSWR from 'swr';
 import styles from '../components/header.module.css'
 
 
 
 export default function Home() {
 
-  const [ session, loading ] = useSession();
-  const [videoId, setVideoId] = useState();
-  const { data, error } = useSWR('/api/billboard', fetcher, {
+ 
+  const [videoId, setVideoId] = useState(null);
+  const [data, setData] = useState([]);
+  const API_URL =
+    "/api/billboard";
 
-    onSuccess: (data) => {
-      if (videoId == undefined)
-        setVideoId(data[0].videoId)
-    }
-  })
 
-  // function getStringDate(data){
-  //   let time = new Date(data[0]);
+  function getData() {
+    setData(null);
+    Axios.get(API_URL).then((res) => {
+      console.log(res.data.slice(0,5))
+      setData(res.data);
+      setVideoId(res.data[0].videoId)
+    })
+  }
 
-  //   console.log(time.getFullYear(), time.getMonth() + 1, time.getDate());
-  //   console.log('시초가', data[1])
-  //   console.log('고가', data[2])
-  //   console.log('저가', data[3])
-  //   console.log('종가', data[4])
-  //   console.log('거래량', data[5])
-  // }
+  // const { data, error } = useSWR('/api/billboard', fetcher, {
 
-  // function getStringDate1(data){
-  //   let time = new Date(data[0]);
+  //   onSuccess: (data) => {
+  //     console.log('data', data)
+  //     if (videoId == undefined)
+  //       setVideoId(data[0].videoId)
+  //   }
+  // })
 
-  //   return console.log(time.getFullYear(), time.getMonth() + 1, time.getDate());
-    
-  // }
-
-  // async function getStaticProps() {
+  useEffect(()=>{
+    getData();
+  },[])
   
-  //   const res = await fetch(`https://api.alphasquare.co.kr/data/v2/price/candle-history/004270?freq=day`)
-  //   const data = await res.json()
-  //   // console.log(data)
-
-  //   // let time = new Date(1605139200000);
-
-  //   // console.log(time.getFullYear(), time.getMonth() + 1, time.getDate());
-
-  //   let c = 0;
-  //   data.forEach(aa=>{
-  //     c += aa[5]
-  //   })
-
-  //   console.log(parseInt( c/data.length));
-
-  //   let temp = data.slice(500,600);
-    
-  //   console.log(temp);
-  //   getStringDate(temp[temp.length -1]);
-    
-  //   let t = temp.filter(t=>t[5] > parseInt( c/data.length)* 10);
-
-  //   console.log(t)
-  //   t.forEach(element => {
-  //     //  getStringDate(element) 
-      
-  //     element[1]
-  //     getStringDate1(element)
-  //     console.log( '이후 눌림목 체크' )
-  //      let tttt = data.find(a => element[0] < a[0] && element[1] > a[4] )
-
-  //      if(tttt != undefined)
-  //      getStringDate(tttt) 
-  //     //  tttt.filter(time => {
-  //     //   getStringDate(time) 
-  //     //  })
-  //     //  console.log(tttt)
-
-  //   });
-
-    
-
-  //   // console.log(t);
-
-    
-
-
-
-    
-  // }
-  // getStaticProps()
 
   const itemTemplate = (data) => {
     return (
@@ -109,9 +56,9 @@ export default function Home() {
         <img src={`${data.image}`} onError={(e) => e.target.src = 'https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png'} alt={data.name}
           onClick={() => {
             setVideoId(data.videoId);
-            let today = new Date();
+            // let today = new Date();
 
-            store.dispatch(addTimeline({ time: today.toLocaleString(), data: data }));
+            // store.dispatch(addTimeline({ time: today.toLocaleString(), data: data }));
           }} />
         <div className="product-detail">
           <div className="product-name">{data.rank}. {data.title}</div>
@@ -132,72 +79,28 @@ export default function Home() {
   if (!data) return <div>Loading...</div>
   return (
 
-
+    
     <Provider store={store}>
-      
+      <React.StrictMode>
       <div className={styles.container}>
         <Head>
-          <title>Create Next App</title>
+          <title>Billboard Chart YouTube</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <main className={styles.main}>
-
-        <div className ="header">
-          {!session && <>
-            <span className={styles.notSignedInText}>You are not signed in</span>
-            <a
-                href={`/api/auth/signin`}
-                className={styles.buttonPrimary}
-                onClick={(e) => {
-                  e.preventDefault()
-                  signIn()
-                }}
-              >
-                Sign in
-              </a>
-          </>}
-          {session && <>
-            {session.user.image && <span style={{backgroundImage: `url(${session.user.image})` }} className={styles.avatar}/>}
-            <span className={styles.signedInText}>
-              <small>Signed in as</small><br/>
-              <strong>{session.user.email || session.user.name}</strong>
-              </span>
-            <a
-                href={`/api/auth/signout`}
-                className={styles.button}
-                onClick={(e) => {
-                  e.preventDefault()
-                  signOut()
-                }}
-              >
-                Sign out
-              </a>
-          </>}
-
-        </div>
         
-
-          <YoutubePlayer videoId={videoId}   />
+          <YoutubePlayer videoId={videoId} data={data}   />
           <div className="datascroller-demo">
             <div className="card">
               <DataScroller value={data} itemTemplate={itemTemplate}
-                rows={5} inline scrollHeight="530px" header="List of Billboard Chart" />
+                rows={10} inline scrollHeight="530px" header="List of Billboard Chart" />
             </div>
 
           </div>
         </main>
-
-        {/* <footer className={styles.footer}>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by{' '}
-            <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-          </a>
-        </footer> */}
+        
       </div>
+      </React.StrictMode>
     </Provider>
   )
 }
